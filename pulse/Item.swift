6a45e1291582e3ed29dcf386e812f744493bc375
Przemyslaw Lusar <lluzak@gmail.com>
2026-01-29 17:47:00 +0100

    Initial Commit

---
+++ pulse/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  pulse
+//
+//  Created by Przemyslaw Lusar on 29/01/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
